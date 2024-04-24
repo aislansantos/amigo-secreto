2b5e2e9063b9bb015b0450565028c6396636e023
Aislan Santos <aislan.santos@gmail.com>
2024-04-24 11:20:21 -0300

    chore: installed zod and use for validation data of logins password

--- src/controllers/Auth.Controller.ts
+++ src/controllers/Auth.Controller.ts
@@ -1,5 +1,16 @@
 import { RequestHandler, Request, Response } from "express";
+import { z } from "zod";
 
 export const login: RequestHandler = (req: Request, res: Response) => {
-  res.json({ teste: "teste" });
+  const loginSchema = z.object({
+    password: z.string(),
+  });
+  const body = loginSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: "Dados inválidos" });
+
+  body.data.password;
+
+  // Validar a senha & gerar o token
+
+  // Retorno da requisição
 };
