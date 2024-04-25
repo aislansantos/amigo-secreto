4626a9e9205e6e529c300d61adf39e9bd0494972
Aislan Santos <aislan.santos@gmail.com>
2024-04-25 13:18:47 -0300

    feat: create controller and service of groups

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import * as AuthController from "@/controllers/Auth.Controller";
 import * as EventsController from "@/controllers/Events.Controller";
+import * as GroupsController from "@/controllers/Groups.Controller";
 import * as AuthMiddleware from "@/middleware/Auth.Middleware";
 
 const router = Router();
-
+// Rota de ping teste
 router.get("/ping", AuthMiddleware.validateToken, AuthController.ping);
+
+// Rota de Login
 router.post("/login", AuthController.login);
+
+// Routas Eventos
 router.get("/events", AuthMiddleware.validateToken, EventsController.getAll);
 router.get(
   "/events/:id",
@@ -23,6 +28,13 @@ router.delete(
   "/events/:id",
   AuthMiddleware.validateToken,
   EventsController.removeEvent,
+);
+
+// Rotas Grupos
+router.get(
+  "/events/:id_event",
+  AuthMiddleware.validateToken,
+  GroupsController.getAll,
 );
 
 export default router;
